20dd0dc50be96d88c27f4b7e89194c1681beb0bc
Jérôme Guzzi <jerome@idsia.ch>
2017-03-21 12:02:51 +0100

    Add temporization to avoid triggering a button too ofter.

---
+++ src/drone_arena/temporized.py
@@ -0,0 +1,16 @@
+import rospy
+
+
+class Temporized(object):
+
+    def __init__(self, period,):
+        self.period = period
+        self.last = None
+
+    def __call__(self, f, *args, **kwargs):
+        def g(*args, **kwargs):
+            if self.last and (rospy.Time.now() - self.last).to_sec() < self.period:
+                return
+            self.last = rospy.Time.now()
+            return f(*args, **kwargs)
+        return g
